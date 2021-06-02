9b33d4e179d062d37edce1007272e5ff413999bf
Mihir Katare <53498695+mihir-katare@users.noreply.github.com>
2021-06-02 18:50:05 -0500

    Initial commit containing reference network implementation and dataloader for random data used in debugging

---
+++ debug/debugdata.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+from torch.utils.data import Dataset
+from torchvision import datasets
+
+class randomNormalGen(Dataset):
+    def __init__(self, n_particles):
+        self.N = 1000
+        self.X = 7 + np.random.randn(self.N, n_particles)
+        self.Y = 20 + np.random.randn(self.N, 1)
+    
+    def __len__(self):
+        return self.N
+    
+    def __getitem__(self, idx):
+        return self.X[idx], self.Y[idx]
---
+++ debug/debugmodel.py
@@ -0,0 +1,62 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from debugdata import randomNormalGen
+from torch.utils.data import Dataset, DataLoader
+
+class referenceNetwork1(nn.Module):
+    ''' 
+    Network based on the description of the reference paper. Assumptions have been made to determine this
+    architecture since an exact description was not given. This Network is the best performing network on the 
+    Drell-Yan Weights.
+    Reference:https://arxiv.org/pdf/2008.10949.pdf
+    '''
+    def __init__(self, n_particles):
+        super(referenceNetwork1, self).__init__()
+        self.fc1 = nn.Linear(n_particles, 200)
+        self.fc2 = nn.Linear(200, 200)
+        self.fc3 = nn.Linear(200, 200)
+        self.fc4 = nn.Linear(200, 200)
+        self.fc5 = nn.Linear(200, 200)
+        self.fc6 = nn.Linear(200, 1)
+        self.relu = nn.ReLU()
+        self.selu = nn.SELU()
+    
+    def forward(self, x):
+        y = self.relu(self.fc1(x))
+        y = self.relu(self.fc2(y))
+        y = self.relu(self.fc3(y))
+        y = self.relu(self.fc4(y))
+        y = self.relu(self.fc5(y))
+        y = self.selu(self.fc6(y))
+        return y
+
+class DNN(nn.Module):
+    def __init__(self):
+        super(DNN, self).__init__()
+        self.n_particles = 6
+        self.net = referenceNetwork1(self.n_particles)
+
+    def train(self):
+        train_data = randomNormalGen(self.n_particles)
+        train_loader = DataLoader(train_data, batch_size=100, shuffle=True)
+        mse = nn.MSELoss()
+        optimizer = torch.optim.Adam(self.net.parameters())
+
+        self.net.train()
+        for epoch in range(20):
+            for i, data in enumerate(train_loader):
+                x_train, y_train = data
+                y_pred = self.net(x_train.float())
+                loss = mse(y_train.float(), y_pred)
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+            print("Epoch: " + str(epoch) + ", Loss: " + str(loss))
+
+obj = DNN()
+obj.train()
